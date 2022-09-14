d9523cf2dcf23056846f2a09820c3c4c2c7d402a
Lutonet <101110383+Lutonet@users.noreply.github.com>
2022-09-14 11:06:40 +0200

    First scratch without unit testing

---
+++ Homework.ClassLibrary/Models/Document.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework.ClassLibrary.Models
+{
+    public class Document
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
